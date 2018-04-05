705d140fca1a3108e51f9e68616c692ecd599e0c
강민승 <bernard@gangminseung-ui-MacBook-Pro.local>
2018-04-05 19:34:24 +0900

    TaceToken ICO, transfer

---
+++ tacs.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.11;
+contract TaceToken{
+    address owner;
+    string public constant name = "Tace Token";
+    string public constant symbol = "TCT";
+    uint8 public constant decimals = 0;
+
+    address public icoAddress;//ICO의 주소
+    mapping (address => uint) public balanceOf;
+
+    event Transfer(address from, address to, uint value);
+
+    function transfer(address _to, uint _value){
+        address _from  = msg.sender;
+        require(_to != address(0));//주소를 입력하지 않은 경우의 예외처리
+        require(balanceOf[_from]>=_value);//잔고가 부족한 경우의 예외처리
+
+        balanceOf[_from] -=_value;
+        balanceOf[_to] += _value;
+        Transfer(_from,_to,_value);
+
+    }
+    function TaceToken(address _icoAddress){
+        owner = msg.sender;
+        icoAddress = _icoAddress;
+        ICO cs = ICO(icoAddress);
+        require(cs.getDeadline()<now);
+        for(uint i=0;i<cs.getIndex();i++){
+            balanceOf[cs.getInvestor(i)]= cs.getTokens(cs.getInvestor(i));
+        }
+    }
+    function killcontract() public{
+            if(owner == msg.sender) selfdestruct(owner);
+    }
+}
+
+contract ICO{
+    mapping (uint => address) investor;
+    mapping (address => uint) public amountInvested;
+
+    address public owner;
+    uint index;
+    uint public constant exchangeRate = 1000;
+    uint public salesStatus;
+    uint deadline;
+    function getIndex() constant returns(uint){
+        return index;
+    }
+    function getInvestor(uint i) constant returns(address){
+        return investor[i];
+    }
+    function getDeadline() constant returns(uint){
+         return deadline;
+    }
+    function getNow() constant returns(uint){
+        return now;
+    }
+    // function getStart() constant returns(uint){
+    //     return start;
+    // }
+    function getTokens(address _investor) constant returns(uint){
+        return amountInvested[_investor]*exchangeRate;
+    }
+    function ICO(uint salesMinutes){
+        owner = msg.sender;
+        deadline = now + salesMinutes * 1 minutes;
+    }
+    function invest() payable{
+        require(now<deadline);
+        if(amountInvested[msg.sender]==0){
+            investor[index] = msg.sender;
+            index++;
+        }
+        amountInvested[msg.sender]+=msg.value;
+        salesStatus += msg.value;
+    }
+    function withdraw(uint amount){
+        if(now > deadline && msg.sender==owner){
+            msg.sender.transfer(amount);
+        }
+    }
+}
